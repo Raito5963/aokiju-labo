2dcc2a241e33722af70db9026ac172fe582b6118
Raito Mochiduki <181489458+Raito5963@users.noreply.github.com>
2024-10-16 22:23:49 +0900

    sf

---
+++ aokiju/pages/news/newsindex.vue
@@ -0,0 +1,20 @@
+<template>
+    <body>
+        <div class="section">
+            <div class="title">
+                <h2>ニュース</h2>
+                <h3>
+                    <p>このサイトや自分に関するニュースをまとめました！</p>
+                </h3>
+            </div>
+            <div class="text">
+                <ul>
+                    <h3>
+                        <li><NuxtLink to="/news/day/20241016">2024/10/13:Ver.2.0稼働！</NuxtLink></li>
+                        <li><NuxtLink to="/news/day/20241013">2024/10/13:Web作成開始！</NuxtLink></li>
+                    </h3>
+                </ul>
+            </div>
+        </div>
+    </body>
+</template>
